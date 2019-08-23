94c7780d64bc489503fb67cae57efa06161ef7cd
Jonathan Ho <johnnyho82@gmail.com>
2019-08-23 11:51:12 -0400

    Create Character and Location components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 
+import CharacterList from './components/CharacterList'
+import LocationsList from './components/LocationsList'
 
 export default function App() {
   return (
     <main>
       <Header />
       {/* <TabNav /> */}
+      {/* <CharacterList /> */}
+      <LocationsList />
     </main>
   );
 }
